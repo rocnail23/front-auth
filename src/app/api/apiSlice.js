8cedc81166c271722503421b1c6c9d216a60e25e
rocnail23 <jdanielrojas16@gmail.com>
2023-06-16 16:48:04 -0400

    sdsd

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL,
+    baseUrl: "https://authserver-p4ae.onrender.com/api/v1/",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().localStorage.token;
         if(token){
